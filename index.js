056e8b4e10c0fdb7c799f691ca290ee8f665fec0
Oluwafunke Dada <odada@Oluwafunkes-MacBook-Air.local>
2024-04-13 02:38:27 -0500

    interacting with apis

--- index.js
+++ index.js
@@ -3,7 +3,26 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+  })
+app.get('/api/program', (req, res) => {
+
+    const programs = [
+{id:1, name: 'Program 1'},
+{id:2, name: 'Program 2'},
+{id:3, name: 'Program 3'},
+ ]
+  res.send(programs)
+})
+
+app.get('/api/users', (req, res) => {
+
+    const users = [
+{id:1, name: 'User 1'},
+{id:2, name: 'User 2'},
+{id:3, name: 'User 3'},
+    ]
+    res.send(users)
 })
 
 app.listen(port, () => {
